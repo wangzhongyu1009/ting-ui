f11a2ad9fc8c168dd7f53d31ea7498699f451aa2
王忠玉 <wangzhongyu@epmworld.net>
2019-04-02 18:26:00 +0800

    'commit'

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 "use strict";
 
-function getNowDate () {
-  console.log(new Date())
+var TING = function () {
+  var aaa = '999'
 }
 
-module.exports = getNowDate;
+TING.prototype.getNowDateStr = function () {
+  return '123';
+}
+
+TING.prototype.getNowDateStr111 = function () {
+  return '222';
+}
+
+module.exports = new TING;
